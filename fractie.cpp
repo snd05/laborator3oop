ac961fea159dde6b1ee05f557d1eec960a20b774
snd05 <teodorescu.alexandru123@gmail.com>
2023-11-02 12:49:44 +0200

    finalizing the implementations

--- fractie.cpp
+++ fractie.cpp
@@ -42,58 +42,89 @@ void fractie::afisare() const {
     cout << "B/A=" << b / a << "\n";
 }
 
-const fractie operator+(const fractie &c, const fractie &d ) {
-    return fractie(c+d);
+const fractie operator+(const fractie &c, const fractie &d) {
+    return fractie(c + d);
 }
 
-const fractie operator-(const fractie& c, const fractie& d){
-    return fractie(c-d);
+const fractie operator-(const fractie &c, const fractie &d) {
+    return fractie(c - d);
 }
 
-const fractie operator-(const fractie &c){
-    int num=-c.a;
-    int den=c.b;
-    return fractie(c.a,c.b);
+const fractie operator-(const fractie &c) {
+    int num = -c.a;
+    int den = c.b;
+    return fractie(c.a, c.b);
 }
 
-const fractie operator*(const fractie &c, const fractie &d){
-    int num = c.a*d.a;
-    int den=c.b*d.b;
-    return fractie(num,den);
+const fractie operator*(const fractie &c, const fractie &d) {
+    int num = c.a * d.a;
+    int den = c.b * d.b;
+    return fractie(num, den);
 }
 
-const fractie operator/(const fractie &c, const fractie &d){
-    int num=c.a/d.a;
-    int den=c.b/d.b;
+const fractie operator/(const fractie &c, const fractie &d) {
+    int num = c.a / d.a;
+    int den = c.b / d.b;
     return fractie(num, den);
 }
 
 void fractie::setData(int c, int d) {
-    this->a=c;
-    this->b=d;
+    this->a = c;
+    this->b = d;
 }
 
-fractie& fractie::operator+=(const fractie &c) {
-    a=a+c.a;
-    b=b+c.b;
+fractie &fractie::operator+=(const fractie &c) {
+    a = a + c.a;
+    b = b + c.b;
     return *this;
 }
 
-fractie& fractie::operator-=(const fractie &c) {
-    a=a-c.a;
-    b=b-c.b;
+fractie &fractie::operator-=(const fractie &c) {
+    a = a - c.a;
+    b = b - c.b;
     return *this;
 }
 
-fractie& fractie::operator*=(const fractie &c) {
-    a=a*c.a;
-    b=b*c.b;
+fractie &fractie::operator*=(const fractie &c) {
+    a = a * c.a;
+    b = b * c.b;
     return *this;
 }
 
-fractie& fractie::operator/=(const fractie &c) {
-    a=a/c.a;
-    b=b/c.b;
+fractie &fractie::operator/=(const fractie &c) {
+    a = a / c.a;
+    b = b / c.b;
     return *this;
 }
 
+bool operator==(const fractie &c, const fractie &d) {
+    return ((c.a == d.a) && (c.b == d.b));
+}
+
+bool operator!=(const fractie &c, const fractie &d){
+    return (!(c==d));
+}
+
+bool operator>(const fractie& x, const fractie& y){
+    double val_x=x.a/x.b;
+    double val_y=y.a/y.b;
+    return (val_x>val_y);
+}
+
+bool operator<(const fractie& x, const fractie& y){
+    double val_x=x.a/x.b;
+    double val_y=y.a/y.b;
+    return (val_x<val_y);
+}
+
+bool operator>=(const fractie& x, const fractie& y){
+    double val_x=x.a/x.b;
+    double val_y=y.a/y.b;
+    return (val_x>=val_y);
+}
+
+bool operator<=(const fractie& x, const fractie& y){
+    double val_x=x.a/x.b;
+    double val_y=y.a/y.b;
+    return (val_x<=val_y);
+}
